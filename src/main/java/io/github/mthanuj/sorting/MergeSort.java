351f4d4a0817d20515939d1141dd77bfa25e9996
mThanuj <mullagurithanuj0@gmail.com>
2026-06-07 08:16:09 +0530

    merge sort

---
+++ src/main/java/io/github/mthanuj/sorting/MergeSort.java
@@ -0,0 +1,51 @@
+package io.github.mthanuj.sorting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeSort {
+
+    public int[] solve(int[] arr) {
+        mergeSort(arr, 0, arr.length - 1);
+
+        return arr;
+    }
+
+    private void mergeSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid + 1, high);
+
+            merge(arr, low, mid, high);
+        }
+    }
+
+    private void merge(int[] arr, int low, int mid, int high) {
+        List<Integer> temp = new ArrayList<>();
+        int left = low;
+        int right = mid + 1;
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left++]);
+            } else {
+                temp.add(arr[right++]);
+            }
+        }
+
+        while (left <= mid) {
+            temp.add(arr[left++]);
+        }
+
+        while (right <= high) {
+            temp.add(arr[right++]);
+        }
+
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+    }
+
+}
---
+++ src/test/java/io/github/mthanuj/sorting/MergeSortTest.java
@@ -0,0 +1,31 @@
+package io.github.mthanuj.sorting;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MergeSortTest {
+
+    private MergeSort mergeSort = new MergeSort();
+
+    @Test
+    void test1() {
+        int[] arr = { 5, 3, 4, 1, 2 };
+
+        int[] expected = { 1, 2, 3, 4, 5 };
+        int[] actual = mergeSort.solve(arr);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void test2() {
+        int[] arr = { 5, 4, 3, 2, 1 };
+
+        int[] expected = { 1, 2, 3, 4, 5 };
+        int[] actual = mergeSort.solve(arr);
+
+        assertArrayEquals(expected, actual);
+    }
+
+}
